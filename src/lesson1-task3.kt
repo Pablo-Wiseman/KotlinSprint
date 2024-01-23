a0b9d15ea5f6d419004700581d14e0469c530842
Pablo-Wiseman <106688042+Pablo-Wiseman@users.noreply.github.com>
2024-01-23 20:54:15 +0500

    lesson1-task3 new variable inserted

---
+++ src/lesson1-task3.kt
@@ -0,0 +1,12 @@
+
+fun main(){
+    val yearOfFlight: Int = 1961
+    var takeOffHour: String = "09"
+    var takeOffMinute: String = "07"
+    takeOffHour = "10"
+    takeOffMinute = "55"
+
+
+    println("$yearOfFlight\n$takeOffHour:$takeOffMinute")
+
+}

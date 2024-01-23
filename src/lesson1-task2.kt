377fbc1aec340cfc0c1c996056d3bca84a185dfd
Pablo-Wiseman <106688042+Pablo-Wiseman@users.noreply.github.com>
2024-01-23 20:15:13 +0500

    lesson1-task2-variable reassigned

---
+++ src/lesson1-task2.kt
@@ -0,0 +1,14 @@
+
+fun main(){
+    val countOfSelectedItems: Int = 50
+    val thanksMessage: String = "Thank you for your purchase"
+    val firedEmployees: Int = 1
+    var totalCountOfEmployees: Int = 2000
+    totalCountOfEmployees = totalCountOfEmployees - firedEmployees
+
+    println("your purchase: $countOfSelectedItems items \n$thanksMessage")
+//    println("Employees: $countOfEmployees persons")
+
+    println(totalCountOfEmployees)
+
+}

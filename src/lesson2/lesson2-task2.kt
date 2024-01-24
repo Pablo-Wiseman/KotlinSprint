abc6ddcf977433560c638c3ab4774ea22162f8d9
Pablo-Wiseman <106688042+Pablo-Wiseman@users.noreply.github.com>
2024-01-24 18:52:07 +0500

    troubleshooting

--- src/lesson2/lesson2-task2.kt
+++ src/lesson2/lesson2-task2.kt
@@ -1,2 +1,21 @@
 package lesson2
 
+fun main() {
+
+    val totalEmployeesCount = 80
+    val newHiredEmployee = 30
+    val tenuredEmployee = 50
+
+    val newHiredEmployeeSalary = 20000
+    val tenuredEmployeeSalary = 50000
+
+
+    val expensesForTenuredEmployees = tenuredEmployee * tenuredEmployeeSalary
+    val expensesForNewHiredEmployees = newHiredEmployee * newHiredEmployeeSalary
+    val totalExpenses = expensesForTenuredEmployees + (newHiredEmployee * newHiredEmployeeSalary)
+    val averageSalaryPerOneEmployee = (expensesForTenuredEmployees + expensesForNewHiredEmployees) / totalEmployeesCount
+
+    println(expensesForTenuredEmployees)
+    println(totalExpenses)
+    println(averageSalaryPerOneEmployee)
+}

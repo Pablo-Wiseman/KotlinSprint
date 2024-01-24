653943070562b12020cb9f1e2033afd97b42c0ac
Pablo-Wiseman <106688042+Pablo-Wiseman@users.noreply.github.com>
2024-01-24 13:43:46 +0500

    lesson2-task1 fractional number printed

---
+++ src/Lesson2/lesson2-task1.kt
@@ -0,0 +1,12 @@
+package Lesson2
+
+fun main(){
+
+    val totalPoints = arrayOf<Byte>(3,4,3,5)
+    val averagePoints = totalPoints.average()
+    val averageFormattedPoints = String.format("%.2f",averagePoints)
+
+    println(averageFormattedPoints)
+
+
+}

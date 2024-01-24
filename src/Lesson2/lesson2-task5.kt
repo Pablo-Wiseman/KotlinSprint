8ffcf9fa8802a3a0a7592b852c397216a6859c39
Pablo-Wiseman <106688042+Pablo-Wiseman@users.noreply.github.com>
2024-01-25 01:09:41 +0500

    KS-2-5-updated

--- src/Lesson2/lesson2-task5.kt
+++ src/Lesson2/lesson2-task5.kt
@@ -3,14 +3,11 @@ package lesson2
 import kotlin.math.pow
 
 fun main(){
-
     val benefit: Double
-
     val deposit = 70_000
     val percent = 16.7
 
     benefit = deposit*(1+percent/100).pow(20)
 
     println(String.format("%.3f", benefit))
-
 }

742f9a82867235602f82d921c6a78e6e29df9932
Pablo-Wiseman <106688042+Pablo-Wiseman@users.noreply.github.com>
2024-01-26 15:14:19 +0500

    KS-2-4-method-updated+percent

--- src/Lesson2/lesson2-task4.kt
+++ src/Lesson2/lesson2-task4.kt
@@ -4,16 +4,17 @@ fun main() {
 
     val crystalOre = 7
     val ironOre = 11
+    val percent = 20
 
-    val ironOreBuff = resourceBuffCount(ironOre)
-    val crystalOreBuff = resourceBuffCount(crystalOre)
+    val ironOreBuff = getResourceCount(ironOre, percent)
+    val crystalOreBuff = getResourceCount(crystalOre, percent)
 
     println("+ $ironOreBuff items to iron ore mined")
     println("+ $crystalOreBuff items to crystal ore mined")
 
 }
 
-fun resourceBuffCount(resourceItems: Int, percent: Int = 20): Int {
+fun getResourceCount(resourceItems: Int, percent: Int): Int {
     val result = resourceItems * percent / 100
     val totalWithBuff = result + resourceItems
     return totalWithBuff - resourceItems
